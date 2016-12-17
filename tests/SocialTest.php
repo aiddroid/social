e5d6f07b989d9cb80f8da108278bcabd11cbd72b
allen <allen@MacBook-Pro-3.local>
2016-12-17 19:54:00 +0800

    init project

---
+++ tests/SocialTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Mockery as m;
+use Symfony\Component\HttpFoundation\Request;
+use Providers\WeiboProviderStub;
+
+
+class SocialTest extends PHPUnit_Framework_TestCase
+{
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testSocialAuthRedirectResponse()
+    {
+        $request = Request::create('foo');
+        $request->setSession($session = m::mock(\Symfony\Component\HttpFoundation\Session\SessionInterface::class));
+        $session->shouldReceive('set')->once();
+
+        $provider = new WeiboProviderStub($request, 'clientId', 'clientSecret', 'redirectUrl', ['display' => 'wap']);
+        $response = $provider->redirect();
+
+        $this->assertInstanceOf(\Symfony\Component\HttpFoundation\RedirectResponse::class, $response);
+        $this->assertSame('http://auth.url', $response->getTargetUrl());
+    }
+}
