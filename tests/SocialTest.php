b7bffeac7a7119175a1f3203ac41eb7791be3c04
aiddroid <aiddroid@gmail.com>
2016-12-17 12:06:47 +0000

    Apply fixes from StyleCI

--- tests/SocialTest.php
+++ tests/SocialTest.php
@@ -1,13 +1,11 @@
 <?php
 
 use Mockery as m;
-use Symfony\Component\HttpFoundation\Request;
 use Providers\WeiboProviderStub;
-
+use Symfony\Component\HttpFoundation\Request;
 
 class SocialTest extends PHPUnit_Framework_TestCase
 {
-
     public function tearDown()
     {
         m::close();
